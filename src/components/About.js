1c8dc430791e8d15a4f322f08be6a3a8bda0a645
unknown <delfdevs@gmail.com>
2019-12-08 22:54:19 -0500

    msg

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const About = () => <h1>About</h1>;
+//const About = () => <h1>About</h1>;
+
+function About () {
+    return <h1>About</h1>
+}
 
 export default About;
